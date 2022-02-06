d49bff95761bcc978c16df97c53d2ef50adc0635
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2022-02-06 18:27:21 +0100

    modif pour le style

--- Game.php
+++ Game.php
@@ -1,22 +1,33 @@
 <?php
 
-class Pendu 
+class Pendu
 {
     public $play;
 
     // Affiche la page d'accueil
-    public function Index(){
+    public function Index()
+    {
 
-        if(!empty($_SESSION['victoires'])){
+        if (!empty($_SESSION['victoires'])) {
 
-            echo "<p'> Bienvenue sur le jeu du Pendu, pour jouer c'est par ici</p>";
-            echo "<a  href='index.php?etat=play'>Continuer</a>";  
-            echo "<a  href='Newgame.php'>Nouvelle partie</a>";  
-        }
-        else{
-
-            echo "<p'> Bienvenue sur le jeu du Pendu, pour jouer c'est par ici</p>";
-            echo "<a  href='Newgame.php'>Nouvelle partie</a>";  
+?>
+            <div class='textalign paddingtop15'>
+                <?php
+                echo "<p class='fontsize-rem1'> Bon retour parmis sur le jeu du Pendu, <br> continuer votre ascenssion ou débuter une autre partie </p>";
+                echo "<a class='new-coontinuerP continuerPos' href='index.php?etat=play'>Continuer</a></p>";
+                echo "<p class='new-coontinuerP nouvellePos'><a href='Newgame.php'>Nouvelle partie</a></p>";
+                ?>
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class='textalign paddingtop15'>
+                <?php
+                echo "<p class='fontsize-rem1'> Bienvenue sur le jeu du Pendu, commencer une partie</p>";
+                echo "<a class='new-coontinuerP nouvellePos2' href='Newgame.php'>Nouvelle partie</a>";
+                ?>
+            </div>
+<?php
         }
     }
 
@@ -24,7 +35,7 @@ class Pendu
     public function choseWord($fichier)
     {
 
-        if(!isset($_SESSION["word"])){
+        if (!isset($_SESSION["word"])) {
             $_SESSION["word"] = rtrim($fichier[array_rand($fichier)]);
         }
     }
@@ -36,14 +47,17 @@ class Pendu
         $delete = ['À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ'];
         $replace = ['A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y'];
 
-        $newWord = str_replace($delete,$replace,$mot);
+        $newWord = str_replace($delete, $replace, $mot);
         return strtoupper($newWord);
     }
 
 
     public function partieGagne($mot)
     {
-        echo "<p> Gagné vous avez réussi à trouver $mot </p> <a href='recommencer.php'>Nouveau Mot</a>";
+        echo " <div class='textalign paddingtop15 fontsize-rem2'>
+        <p> Gagné vous avez réussi à trouver le mot <br></p>
+        <p class=' motcorrect'> $mot</p>  <a class='new-coontinuerP recommencerPos' href='recommencer.php'>Nouveau Mot</a>
+        </div>";
         $_SESSION['victoires']++;
         echo $_SESSION['victoires'];
         exit();
@@ -51,31 +65,28 @@ class Pendu
 
     public function partiePerdue($mot)
     {
-        echo " <p>Perdu le mot était $mot </p> <a href='recommencer.php'>Recommencer</a>";
+        echo " <div class='textalign paddingtop15 fontsize-rem2'>
+        <p>Perdu le mot était <br></p>
+        <p class=' motcorrect'> $mot </p>  <a class='new-coontinuerP recommencerPos' href='recommencer.php'>Recommencer</a>
+        </div>";
         exit();
     }
-
 
     public function Affichage($lettres)
     {
 
-        for($i=0; isset($lettres[$i]);$i++)
-            {
+        for ($i = 0; isset($lettres[$i]); $i++) {
 
-                if(!empty($this->play) && in_array($lettres[$i], $this->play)) 
-                {
+            if (!empty($this->play) && in_array($lettres[$i], $this->play)) {
 
-                    echo "";
-                }
-                else
-                {
+                echo "";
+            } else {
 
-                    echo '<input type="submit" name="lettres" value="'.$lettres[$i].'">';
-                }
+                echo '<input type="submit" name="lettres" value="' . $lettres[$i] . '">';
             }
+        }
+    }
 
-    }    
-    
     // Je stocke les lettres jouées dans une variable de session
     public function lettresJouees()
     {
@@ -90,28 +101,27 @@ class Pendu
 
         $played = $_SESSION['played'];
         $this->play = $played;
-        for($i=0;isset($played[$i]);$i++){
+        for ($i = 0; isset($played[$i]); $i++) {
 
-            if(!in_array($played[$i],str_split($mot))){ /*str_plit Transforme mon mot en tableau */
+            if (!in_array($played[$i], str_split($mot))) { /*str_plit Transforme mon mot en tableau */
 
                 $_SESSION['false']++;
-                
             }
         }
     }
 
 
-        public function bonnesLettres($mot)
-        {
-            for($j=0;isset($mot[$j]);$j++){
+    public function bonnesLettres($mot)
+    {
+        for ($j = 0; isset($mot[$j]); $j++) {
 
-                if(!empty($this->play) && in_array($mot[$j],$this->play)){
+            if (!empty($this->play) && in_array($mot[$j], $this->play)) {
 
-                    $_SESSION['true']++;
-                    echo $mot[$j];
-                }else{
-                    echo " _ ";
-                }
+                $_SESSION['true']++;
+                echo $mot[$j];
+            } else {
+                echo " _ ";
             }
         }
+    }
 }
--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     include "header.php";
     ?>
     <main>
-
-        <?php
+        <div class="Alphabet">
+            <?php
             include "Game.php";
             session_start();
             $fichier = file("mots.txt");
-            $lettres = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+            $lettres = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
             $game = new Pendu;
             $game->choseWord($fichier);  /*Recuperation d'un mot au hasard dans le fichier mots.txt */
             $maj = strtoupper($_SESSION["word"]); /* mot en maj */
@@ -24,78 +26,73 @@
 
             $_SESSION['true'] = 0;
             $_SESSION['false'] = 0;
-            
 
-            if(!isset($_SESSION['victoires'])){
+
+            if (!isset($_SESSION['victoires'])) {
                 $_SESSION['victoires'] = 0;
                 $victoires = $_SESSION['victoires'];
             }
 
-
-            if(empty($_GET)){
+            if (empty($_GET)) {
 
                 // Si je n'ai rien dans l'url affichage index hors jeu
                 $game->Index();
+            } elseif (!empty($_GET) && $_GET['etat'] == 'play') {
+                /** Si j'ai ce qu'il faut dans l'url affichage */
 
+                if (isset($_POST['lettres'])) {
 
-            } elseif(!empty($_GET) && $_GET['etat']=='play'){ /** Si j'ai ce qu'il faut dans l'url affichage */
-                
-                
-
-
-                if(isset($_POST['lettres'])){
-    
                     $game->lettresJouees();
                 }
 
-
-                if(!empty($_SESSION['played'])){
+                if (!empty($_SESSION['played'])) {
 
                     $game->mauvaisesLettres($mot);
-                    
                 }
 
-                ?>
-                <form action="" method="post">
-                    <?php $game->Affichage($lettres); ?>
-                </form>
-                <?php  
+            ?>
+                <div class="textalign paddingtop5 box paddingbottom4">
+                    <form action="" method="post">
+                        <?php $game->Affichage($lettres); ?>
+                    </form>
 
-                $game->bonnesLettres($mot);
-                $hang = $_SESSION['false'] -1;
-                echo '<img src="img/Hangman-'.$hang.'.png"';
-                $false=$_SESSION['false'];
-                $true=$_SESSION['true'];
+                    <?php
+
+                    $hang = $_SESSION['false'] - 1;
+                    echo '<img src="img/Hangman-' . $hang . '.png"<br>';
+
+                    $game->bonnesLettres($mot);
+
+                    $false = $_SESSION['false'];
+                    $true = $_SESSION['true'];
+
+                    if ($false >= 7) {
+                        header("Refresh:1; url=index.php?etat=lose");
+                        // header("location:index.php?etat=lose");
+                    } elseif ($true == strlen($mot)) {
+                        header("Refresh:1; url=index.php?etat=win");
+                        // header("location:index.php?etat=win");
+                    }
+
+                    ?>
+</div>
+                    <a class="RecomPos" href="index.php">Recommencer</a>
                 
-                if($false >= 7){
+        </div>
+    <?php
 
-                    header("location:index.php?etat=lose");
-
-                } elseif($true==strlen($mot)){
-
-                    header("location:index.php?etat=win");
-                    
-                }
-
-                ?>
-                
-                <a href="index.php">Recommencer</a>
-
-            <?php
-                    
-            }elseif(!empty($_GET) && $_GET['etat']=='win'){
+            } elseif (!empty($_GET) && $_GET['etat'] == 'win') {
 
                 $game->partieGagne($mot);
-                
-
-            }elseif(!empty($_GET) && $_GET['etat']=='lose'){
+            } elseif (!empty($_GET) && $_GET['etat'] == 'lose') {
 
                 $game->partiePerdue($mot);
-
             }
-            
-    ?>   
+
+    ?>
+    </div>
     </main>
     <a href=""></a>
 </body>
+
 </html>
